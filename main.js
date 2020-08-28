067e790482c2207b27d60b666f2c5a177676dc85
Valerio <valerio.modesti@gmail.com>
2020-08-28 15:40:05 +0200

    init2

--- main.js
+++ main.js
@@ -1,3 +1,28 @@
 $(document).ready(function() {
-	//Code
+    $.ajax(
+    {
+         'url': 'https://flynn.boolean.careers/exercises/api/array/music',
+         'method': 'GET',
+         'success': function(arrObj){
+                  console.log(arrObj);
+                  
+                  for (var i = 0; i < arrObj.length; i++) {
+                     leggiDati(arrObj.poster);
+
+
+                  }
+         },
+         'error':function(){
+           alert('errore!');
+         }
+    }
+    )
 });
+
+function leggiDati (d){
+	var source = $('#entry-template').html();
+	var template = Handlebars.compile(source);
+
+	var html = template(d);
+	$('.cds-container').append(html);
+}
