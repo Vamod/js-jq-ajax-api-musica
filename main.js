f7ddd18e5a98ec9c1333377fea26b7b2496d787b
Valerio <valerio.modesti@gmail.com>
2020-08-31 00:51:04 +0200

    parte bonus

--- main.js
+++ main.js
@@ -12,8 +12,75 @@ $(document).ready(function() {
                   for (var i = 0; i < arr.length; i++) {
                      console.log(arr[i]);
                      leggiDati(arr[i]);
-                  }
+                 }
+
+                 $('#pop').click(function(){
+                     $('.cd').hide();
+                     var i = 0;
+
+                     while (i < arr.length){
+                         var genere = arr[i].genre;
+                            if(genere == 'Pop'){
+                                leggiDati(arr[i]);
+                            }
+
+                         i++;
+                     }
+                 });
+
+                 $('#rock').click(function(){
+                     $('.cd').hide();
+                     var i = 0;
+
+                     while (i < arr.length){
+                         var genere = arr[i].genre;
+                            if(genere == 'Rock'){
+                                leggiDati(arr[i]);
+                            }
+
+                         i++;
+                     }
+                 });
+
+                 $('#metal').click(function(){
+                     $('.cd').hide();
+                     var i = 0;
+
+                     while (i < arr.length){
+                         var genere = arr[i].genre;
+                            if(genere == 'Metal'){
+                                leggiDati(arr[i]);
+                            }
+
+                         i++;
+                     }
+                 });
+
+                 $('#jazz').click(function(){
+                     $('.cd').hide();
+                     var i = 0;
+
+                     while (i < arr.length){
+                         var genere = arr[i].genre;
+                            if(genere == 'Jazz'){
+                                leggiDati(arr[i]);
+                            }
+
+                         i++;
+                     }
+                 });
+
+                 $('#all').click(function(){
+
+                     for (var i = 0; i < arr.length; i++) {
+                        leggiDati(arr[i]);
+                    }
+
+                 });
+
+
          },
+
          'error':function(){
            alert('errore!');
          }
